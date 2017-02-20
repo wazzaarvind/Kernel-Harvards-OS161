b42df0eb9b1707aa22c4a9a5aefb1ebd733cd011
Arvind <wazzaarvind@gmail.com>
2017-02-20 17:04:42 +0000

    2.1 start

--- kern/syscall/file_syscall.c
+++ kern/syscall/file_syscall.c
@@ -1,5 +1,8 @@
 int sys_write(int fd, const void *buf,size_t size, ssize_t *retval){
 	struct uio uio;
+	if(fd<0||fd>OPEN_MAX)
+		return EBDAF; //googled
 	//check if FD invalid, return EBDAF
-	
+
+	//check when address space pointed by buf is invalid, return EFAULT	
 }
