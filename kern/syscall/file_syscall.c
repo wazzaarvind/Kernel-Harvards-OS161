c3af8bfb2864bb3c6be1959b4ba161c0e530138a
Arvind <wazzaarvind@gmail.com>
2017-03-02 16:36:00 +0000

    Write changed

--- kern/syscall/file_syscall.c
+++ kern/syscall/file_syscall.c
@@ -19,7 +19,7 @@ int sys_write(int fd, const void *buf,size_t size, ssize_t *retval){
   //check if FD invalid, return EBDAF
   if(fd<0||fd>OPEN_MAX)
   	return EBADF;
- 
+
   int result = 0;
 
   /*Achuth edits : Fetching the stdout file handle and writing to the file.*/
@@ -30,15 +30,15 @@ int sys_write(int fd, const void *buf,size_t size, ssize_t *retval){
 
   outFile = curproc->filetable[1]->file;
 
-  //lock_acquire(curproc->filetable[fd]->lock);
+  lock_acquire(curproc->filetable[fd]->lock);
   iov->iov_ubase = (void *)buf;
   iov->iov_len = size;
 
   uioWrite->uio_iov = iov;
   uioWrite->uio_iovcnt = 1;
   uioWrite->uio_offset = 0;
-  //uioWrite->uio_offset = curproc->filetable[fd]->offset;
-  uioWrite->uio_resid = size;
+  uioWrite->uio_offset = curproc->filetable[fd]->offset;
+  //uioWrite->uio_resid = size;
   uioWrite->uio_segflg = UIO_USERSPACE;
   uioWrite->uio_rw = UIO_WRITE;
   uioWrite->uio_space = curproc->p_addrspace;
@@ -46,15 +46,15 @@ int sys_write(int fd, const void *buf,size_t size, ssize_t *retval){
   int err = VOP_WRITE(outFile, uioWrite);
 
   if(err){
-	//lock_release(curproc->filetable[fd]->lock);
-	//return err;
+	lock_release(curproc->filetable[fd]->lock);
+	return err;
   }
-  /*curproc->filetable[fd]->offset=uioWrite->uio_offset;
+  curproc->filetable[fd]->offset=uioWrite->uio_offset;
   *retval = size - uioWrite->uio_resid;
-  lock_release(curproc->filetable[fd]->lock);*/
+  lock_release(curproc->filetable[fd]->lock);
 
 
-  retval++;
+  //retval++;
 
 	return result;
 	//check when address space pointed by buf is invalid, return EFAULT
